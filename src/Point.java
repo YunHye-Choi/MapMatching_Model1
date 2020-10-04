f57a9544276536f091a70adea4fefacb5965b167
yoooolim <agada937@naver.com>
2020-10-05 02:20:02 +0900

    vector2d commit

--- src/Point.java
+++ src/Point.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Vector;
 
 public class Point {
     private Double x; // x좌표
@@ -37,9 +38,33 @@ public class Point {
         this.y = y;
     }
 
-    public ArrayList<Integer> findRadiusLink(ArrayList<Link> linkArrayList){
+    public ArrayList<Integer> findRadiusLink(ArrayList<Link> linkArrayList, ArrayList<Node> nodeArrayList){
         int Radiusnum = 2;
         ArrayList<Integer> RadiusLinkID = new ArrayList<>();
+        for(int i=0;i<linkArrayList.size();i++){//원 안에 시작,끝점이 있는 경우
+            if(this.coordDistance(nodeArrayList.get(linkArrayList.get(i).getStartNodeID()).getCoordinate())<Radiusnum||
+                    this.coordDistance(nodeArrayList.get(linkArrayList.get(i).getEndNodeID()).getCoordinate())<Radiusnum)
+                RadiusLinkID.add(linkArrayList.get(i).getLinkID());
+            else{// 원 안에 시작, 끝점이 없는 경우
+                double startX = nodeArrayList.get(linkArrayList.get(i).getStartNodeID()).getCoordinate().getX();
+                double startY = nodeArrayList.get(linkArrayList.get(i).getStartNodeID()).getCoordinate().getY();
+                double endX = nodeArrayList.get(linkArrayList.get(i).getEndNodeID()).getCoordinate().getX();
+                double endY = nodeArrayList.get(linkArrayList.get(i).getEndNodeID()).getCoordinate().getY();
+
+                double inclination = (startY-endY)/(startX-endX);
+                double Y_Intercept = -(inclination*startX)+startY;
+
+                //원의 중심 부터 직선 사이의 거리가 반지름보다 작은 경우
+                if(Math.abs(inclination*this.getX()-this.getY()+Y_Intercept)/Math.sqrt(Math.pow(inclination,2)+1)<Radiusnum) {
+                    //원의 중심 부터 직선까지의 거리가 r보다 작다면 다음 식에 들어오게 된다.
+                    Vector2D vectorFromCircle = new Vector2D(startX-this.getX(),startY-this.getY());
+                    Vector2D vectorLine1 = new Vector2D(startX-endX,startY-endY);
+                    Vector2D vectorLine2 = new Vector2D(endX-startX,endY-startY);
+                    if(Math.acos(vectorFromCircle.dot(vectorLine1))>=90&&Math.acos(vectorFromCircle.dot(vectorLine2))>=90)
+                        RadiusLinkID.add(linkArrayList.get(i).getLinkID());
+                }
+            }
+        }
         return RadiusLinkID;
     }//반경 내에 존재하는 link ID구하기
 
